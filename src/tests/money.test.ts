14b08f82c2ed36df75335c133584dc7d7263490c
gn-t-k <gntk1995422@gmail.com>
2021-02-12 16:26:36 +0900

    レッド

--- src/tests/money.test.ts
+++ src/tests/money.test.ts
@@ -5,5 +5,7 @@ describe('Dollar', () => {
     const five = new Dollar(5);
     five.times(2);
     expect(five.amount).toEqual(10);
+    five.times(3);
+    expect(five.amount).toEqual(15);
   });
 });
